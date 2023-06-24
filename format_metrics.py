6e1bded86e3eb9ea358777fbd1adf6969c9785ec
Asad-Ismail <asadismaeel@gmail.com>
2023-06-24 22:54:05 +0000

    updated metrics

--- format_metrics.py
+++ format_metrics.py
@@ -1,11 +1,16 @@
+import json
 import pandas as pd
+import sys
 
 def format_metrics(filename):
-    df = pd.read_csv(filename)
-    # adjust column width
-    #pd.set_option('display.max_colwidth', 100)  # adjust as needed
-    return df.to_html()
+    with open(filename, "r") as read_file:
+        data = json.load(read_file)
+
+    df = pd.json_normalize(data)
+    df.index = ["Metrics"]
+    pd.set_option('display.max_colwidth', -1)
+    return df.to_markdown()
 
 if __name__ == "__main__":
-    import sys
     print(format_metrics(sys.argv[1]))
+
