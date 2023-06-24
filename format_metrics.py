bb5767e50a0cb6d3ae7bd1a1b6620cd4a9f7b535
Asad-Ismail <asadismaeel@gmail.com>
2023-06-24 22:41:31 +0000

    fixed format

--- format_metrics.py
+++ format_metrics.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 def format_metrics(filename):
     df = pd.read_csv(filename)
+    # adjust column width
+    pd.set_option('display.max_colwidth', 100)  # adjust as needed
     return df.to_markdown()
 
 if __name__ == "__main__":
