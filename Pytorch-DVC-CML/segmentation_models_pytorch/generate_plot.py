0806080ece636594a8c0f27409227c4616c9fe0e
Asad-Ismail <asadismaeel@gmail.com>
2023-06-25 13:15:04 +0000

    updated report

--- Pytorch-DVC-CML/segmentation_models_pytorch/generate_plot.py
+++ Pytorch-DVC-CML/segmentation_models_pytorch/generate_plot.py
@@ -7,7 +7,7 @@ def plot_metrics(metrics_file, title):
     
     df.plot()
     plt.title(title)
-    plt.savefig(f'Pytorch-DVC-CML/segmentation_models_pytorch/cml_reports/{title}.png')
+    plt.savefig(f'{title}.png')
 
 if __name__ == "__main__":
     plot_metrics(sys.argv[1], sys.argv[2])
