a80d1ec3257dccf631dd4bf4f2828a75f8ab44de
Asad-Ismail <asadismaeel@gmail.com>
2023-06-26 16:35:07 +0000

    updated inference per image

--- Pytorch-DVC-CML/segmentation_models_pytorch/infer_image.py
+++ Pytorch-DVC-CML/segmentation_models_pytorch/infer_image.py
@@ -9,14 +9,17 @@ from segmentation_model import PetModel
 
 # Load your trained model
 model = PetModel("FPN", "resnet34", in_channels=3, out_classes=1)
-checkpoint_path = "checkpoints/model-best.ckpt"
+checkpoint_path = "modelCheckpoints/model-best.ckpt"
 model.load_state_dict(torch.load(checkpoint_path)["state_dict"])
 
-file="pets_data/images/english_cocker_spaniel_141.jpg"
+img_path="pets_data/images/english_cocker_spaniel_141.jpg"
 
-image = np.array(Image.fromarray(file).resize((256, 256), Image.LINEAR))
+image = np.array(Image.open(img_path).convert('RGB').resize((256, 256), Image.BILINEAR))
+vis_img=image.copy()
 image=np.moveaxis(image, -1, 0)
 
+# Define a color map for masks
+color_map = [[0, 0, 128], [0, 128, 0], [128, 0, 0]]  # Blue, Green, Red
 
 # Add batch dimension and send image through your model
 model.eval()
@@ -31,10 +34,11 @@ pr_mask = np.where(pr_mask<0.5,0,1).astype('uint8')
 
 # Convert masks to RGB and add a color map
 pr_mask_rgb = cv2.cvtColor(pr_mask, cv2.COLOR_GRAY2BGR)
+
 for c in range(3):
     pr_mask_rgb[:, :, c] = np.where(pr_mask_rgb[:, :, c] > 0, color_map[2][c], 0)
 
 # Overlay masks on the original image
-overlay_pr = cv2.addWeighted(image, 0.7, pr_mask_rgb, 0.3, 0)
+overlay_pr = cv2.addWeighted(vis_img, 0.4, pr_mask_rgb, 0.6, 0)
 
 cv2.imwrite(f"test.png",overlay_pr)
